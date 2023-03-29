deefd843f9c4d069303e3e7cc3b9a9b79cbe7dc5
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-03-29 20:16:29 +0100

    removed unused break; in switch statment after return call

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,16 +4,12 @@ function renderLicenseBadge(license) {
   switch(license) {
     case 'MIT':
       return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      break;
     case 'APACHE 2.0':
       return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-      break;
     case 'GPL 3.0':
       return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-      break;
     default:
       return "";
-      break;
   }
 }
 
